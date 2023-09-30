e24b19ce27adc1324022df71b54bb7b07cbcdd06
Ramdas Gupta <er.rdgupta@gmail.com>
2023-09-30 09:12:12 +0530

    sequelize updated

--- app/models/index.js
+++ app/models/index.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 })
 
 const db = {};
--- index.js
+++ index.js
@@ -24,7 +24,12 @@ db.sequelize.sync().then(() => {
 
 // Middleware
 app.use(json())
+
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization, My-custom-header');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     console.log(chalk.green(req.method), chalk.blue(req.url))
     next();
 });
